a7619150307039ed70734f6a838b67e27b39e52b
Drew Miller <poppiestar@users.noreply.github.com>
2019-01-16 12:47:45 +0000

    Start making drone home page, remove redundant image

--- src/src/components/templates/home/home.template.jsx
+++ src/src/components/templates/home/home.template.jsx
@@ -1,21 +1,12 @@
 // @flow
 import React from "react";
-import logo from './logo.svg';
 import './home.template.css';
 
 const HomeTemplate = () => (
     <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>Edit <code>src/App.js</code> and save to reload.</p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
+            <h1>Drone Home</h1>
+            <p>We like drones, you like drones. Take a look at our drones!</p>
         </header>
     </div>
 );
