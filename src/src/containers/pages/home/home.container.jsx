647ecb2511f305961937efa23938754dc977f2b3
Drew Miller <poppiestar@users.noreply.github.com>
2019-01-16 13:10:01 +0000

    Convert home container to class component to prepare for data retrieval

--- src/src/containers/pages/home/home.container.jsx
+++ src/src/containers/pages/home/home.container.jsx
@@ -1,9 +1,15 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { HomeTemplate } from "../../../components/templates";
 
-const HomeContainer = () => (
-    <HomeTemplate drones={[]} />
-);
+class HomeContainer extends Component<{}> {
+    render() {
+        return (
+            <HomeTemplate
+                drones={[]}
+            />
+        );
+    }
+}
 
 export default HomeContainer;
